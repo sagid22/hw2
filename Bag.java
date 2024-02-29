2c7ef2ba7188369cd73dee5433e27ed7a528b87c
barak <baraksharon10@gmail.com>
2024-02-29 11:52:25 +0200

    HW3 begin1

--- Bag.java
+++ Bag.java
@@ -3,7 +3,6 @@ public class Bag extends Item{
     int value;
     int cap;
     Item[] inventory;
-
     public Bag(String name, int value, int cap){
         super(name, value);
         this.inventory=new Item[cap];
